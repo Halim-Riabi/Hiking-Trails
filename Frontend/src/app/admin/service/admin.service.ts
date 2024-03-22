6e36e7e2460b67a380d7dad2722528cb5b61bd46
guyroW <halimriabi001@gmail.com>
2024-03-22 02:17:32 +0100

    creating post category page [P1]

---
+++ Frontend/src/app/admin/service/admin.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8085/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  addCategory(categoryDto:any): Observable<any>{
+    return this.http.post(BASIC_URL + 'api/admin/category', categoryDto,{
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
+  private createAuthorizationHeader(): HttpHeaders{
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer ' + UserStorageService.getToken()
+    )
+  }
+}
