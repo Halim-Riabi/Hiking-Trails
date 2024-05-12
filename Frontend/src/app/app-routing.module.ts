3c38ad4f4fb31dd6036a6a1f3737e29548c9591e
guyroW <halimriabi001@gmail.com>
2024-05-12 17:45:04 +0100

    setting up the default starting page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { VisitorComponent } from './visitor/visitor.component';
+import { DashboardComponent } from './visitor/components/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/visitor', pathMatch: 'full' },
   { path : "login", component: LoginComponent},
+  { path : "visitor", component: DashboardComponent},
   { path : "signup", component: SignupComponent},
   { path: 'hiker', loadChildren: () => import('./hiker/hiker.module').then(m => m.HikerModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
